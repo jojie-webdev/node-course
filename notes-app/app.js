1c604317c5f892ffb9cec884b189f40d21779dab
Jojie Jagonos <jojiejagonos@elink.corp>
2019-03-11 17:18:41 +0900

    Notes done

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,78 @@
 const validator = require('validator')
 const chalk = require('chalk')
-const getNotes = require('./notes.js')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-const notes = getNotes()
+//Customize yargs version
+yargs.version('1.1.0')
 
-console.log(notes)
 
-console.log(validator.isEmail('andre@example.com'))
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List new note',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+
+//add, remove, read, list
+
 
 console.log(chalk.blue.bold('Hello world!'));
 
-
-// const add = require('./utils.js');
-// const sum = add(4, -2)
-// console.log(sum);
+yargs.parse()
