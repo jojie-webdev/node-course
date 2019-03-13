17b67c4b24666a7bb191fd7f32e71e77fc69e322
Jojie Jagonos <jojiejagonos@elink.corp>
2019-03-13 16:26:59 +0900

    Setup view engine and static directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,37 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+//Setup static directory to serve
+app.use(express.static(path.join(publicDirectoryPath)))
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
-})
-
-app.get('/help', (req, res) => {
-    res.send('Help Page!')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Jojie Jagonos'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page!')
+    res.render('about', {
+        title: 'About Me',
+        name: 'Jojie Jagonos'
+    })
 })
 
-app.get('/weather', (req, res) => {
-    res.send('Weather Page!')
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: 'I want to be a developer please help me',
+        name: 'Jojie Jagonos'
+    })
 })
 
 app.listen(3000, () => {
